876c8feb31fc4bcf2341888ae613267a74b71d54
Forgettttts <alan.zapata@usm.cl>
2021-06-12 22:07:06 -0400

    work

--- try.py
+++ try.py
@@ -1,13 +1,22 @@
-""""regiones_archivo = open("RegionesComunas.csv", "r")
-comunas_archivo = open("CasosConfirmadosPorComuna.csv", "r")
+from prettytable import PrettyTable
+import cx_Oracle
 
-for linea_leida in regiones_archivo:
-    NombreRegion, CodigoRegion, CodigoComuna = linea_leida.strip(
-        "\n").split(",")
-    print(NombreRegion)
-    break
-"""
-perro="Aysen"
-lista=[]
-lista.append(perro)
-print(perro, perro.upper())
+# Nos conectamos como el usuario todo con password "123" al localhost y puerto 1521.
+connection = cx_Oracle.connect("TODO", "123", "localhost:1521")
+print("Database version:", connection.version)
+cursor = connection.cursor()
+
+def combinar_comuna(CodigoPrimero, CodigoSegundo, CodigoAMantener):
+    if (len(CodigoPrimero) == 4):
+        CodiRegi1 = CodigoPrimero[0:1]
+    elif(len(CodigoPrimero) == 5):
+        CodiRegi1 = CodigoPrimero[0:2]
+    if (len(CodigoSegundo) == 4):
+        CodiRegi2 = CodigoSegundo[0:1]
+    elif(len(CodigoSegundo) == 5):
+        CodiRegi2 = CodigoSegundo[0:2]
+    if CodiRegi1 == CodiRegi2:  # Las comunas pertenecen a regiones distintas
+        if CodigoAMantener == 1:
+            cursor.execute(
+                """SELECT * FROM CASOS_POR_COMUNA WHERE CodigoComuna=:1""", [str(CodigoSegundo)])
+            print(cursor.fetchall())
