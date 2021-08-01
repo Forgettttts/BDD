52569b21b3c3d769d6687321b03a2b669570c6ba
Forgettttts <alan.zapata@usm.cl>
2021-08-01 03:40:01 -0400

    avance sabado

--- Tarea2/registrarse.php
+++ Tarea2/registrarse.php
@@ -13,8 +13,34 @@ include_once'header.php'
                 <input type="password" name="password" placeholder="Elija contraseña">
                 <input type="password" name="password2" placeholder="Repita su contraseña">
                 <button type="submit" name="submit">Registrarme</button>
+            <?php
+            if (isset($_GET["error"])){
+                if (($_GET["error"])=="EntradaVacia"){
+                    echo "<p>Por favor, rellena todos los campos.</p>";
+                }
+                else if (($_GET["error"])=="UsuarioInvalido"){
+                    echo "<p>Por favor, ingresa un nombre de usuario válido.</p>";
+                }
+                else if (($_GET["error"])=="CorreoInvalido"){
+                    echo "<p>Por favor, ingresa un correo válido.</p>";
+                }
+                else if (($_GET["error"])=="PasswordDistintas"){
+                    echo "<p>Las contraseñas no coinciden.</p>";
+                }
+                else if (($_GET["error"])=="UsuarioYaExistente"){
+                    echo "<p>Nombre de usuario ya existente, por favor, prueba con otro.</p>";
+                }
+                else if (($_GET["error"])=="ConexionFallida"){
+                    echo "<p>Algo salió mal, por favor, intenta nuevamente.</p>";
+                }
+                else if (($_GET["error"])=="none"){
+                    echo "<p>Registro exitoso!.</p>";
+                }
+            }
+            ?>
             </form>
         </div>
+        
     </section>
 </div>
 
